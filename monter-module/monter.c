f30f6b9fd4961df76301f130af3b897647298698
Dariusz Sosnowski <dar.sosnowski@gmail.com>
2017-05-02 17:46:55 +0200

    monter: single cdev allocation

--- monter-module/monter.c
+++ monter-module/monter.c
@@ -1,40 +1,145 @@
 /*
+TODO(sodar):
+
+- Support multiple Monter devices
+	- Dynamic allocation of minor numbers
+*/
+
+/*
  * Author: Dariusz Sosnowski <ds384373@students.mimuw.edu.pl>
  */
+#include <linux/cdev.h>
+#include <linux/fs.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/pci.h>
+#include <linux/spinlock.h>
 
 #include "monter.h"
 #include "monter_ioctl.h"
 
+#define DRIVER_NAME "monter"
+#define MAX_DEVICES (256)
+
 MODULE_AUTHOR("Dariusz Sosnowski <ds384373@students.mimuw.edu.pl>");
-MODULE_DESCRIPTION("monter device driver");
+MODULE_DESCRIPTION("ZSO: task #2: monter device driver");
 MODULE_LICENSE("GPL");
 
-static unsigned int next_chr_dev_num = 0;
+struct monter_device_context {
+	struct cdev cdev;
+	dev_t devt;
+};
+
+static dev_t monter_dev_numbers;
+static struct class *monter_dev_class;
+
+static ssize_t
+monter_fops_write(struct file *file, const char __user *input,
+		  size_t count, loff_t *off)
+{
+	printk(KERN_INFO "monter: %s called\n", __func__);
+
+	/* TODO(sodar): Implement */
+
+	return count;
+}
+
+static long
+monter_fops_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
+{
+	printk(KERN_INFO "monter: %s called\n", __func__);
+
+	/* TODO(sodar): Implement */
+
+	return -EINVAL;
+}
+
+static int
+monter_fops_mmap(struct file *file, struct vm_area_struct *vm)
+{
+	printk(KERN_INFO "monter: %s called\n", __func__);
+
+	/* TODO(sodar): Implement */
+
+	return -EINVAL;
+}
+
+static int
+monter_fops_open(struct inode *inode, struct file *file)
+{
+	printk(KERN_INFO "monter: %s called\n", __func__);
+
+	/* TODO(sodar): Implement */
+
+	return 0;
+}
+
+static int
+monter_fops_release(struct inode *inode, struct file *file)
+{
+	printk(KERN_INFO "monter: %s called\n", __func__);
+
+	/* TODO(sodar): Implement */
+
+	return 0;
+}
+
+static int
+monter_fops_fsync(struct file *file, loff_t off1, loff_t off2, int datasync)
+{
+	printk(KERN_INFO "monter: %s called\n", __func__);
+
+	/* TODO(sodar): Implement */
+
+	return -EINVAL;
+}
+
+static struct file_operations monter_fops = {
+	.owner		= THIS_MODULE,
+	.write		= monter_fops_write,
+	.unlocked_ioctl	= monter_fops_ioctl,
+	.mmap		= monter_fops_mmap,
+	.open		= monter_fops_open,
+	.release	= monter_fops_release,
+	.fsync		= monter_fops_fsync,
+};
 
 static int
 monter_pci_probe(struct pci_dev *dev, const struct pci_device_id *id)
 {
-	/*
-	 * TODO(sodar):
-	 * - create character device for attached device
-	 *   - select device number from range <0,255>
-	 */
+	struct monter_device_context *ctx;
+	struct device *device;
+	dev_t cdev_major;
+	dev_t cdev_minor;
+	dev_t cdev_numbers;
+	int ret;
 
-	unsigned int *chr_dev_num;
-
-	chr_dev_num = (unsigned int *)kmalloc(sizeof(*chr_dev_num), GFP_KERNEL);
-	if (chr_dev_num == NULL) {
-		return -ENODEV;
+	ctx = kzalloc(sizeof(*ctx), GFP_KERNEL);
+	if (ctx == NULL) {
+		printk(KERN_ERR "%s: out of memory\n", DRIVER_NAME);
+		return -ENOMEM;
 	}
 
-	*chr_dev_num = next_chr_dev_num++;
-	next_chr_dev_num %= 256;
-	pci_set_drvdata(dev, chr_dev_num);
+	cdev_init(&ctx->cdev, &monter_fops);
+	ctx->cdev.owner = THIS_MODULE;
 
-	printk(KERN_INFO "monter: PCI device probe; chr_dev_num = %u\n", *chr_dev_num);
+	cdev_major = MAJOR(monter_dev_numbers);
+	cdev_minor = MINOR(monter_dev_numbers);
+	cdev_numbers = MKDEV(cdev_major, cdev_minor + 1); // TODO(sodar): For the beginning, /dev/monter1
+	ctx->devt = cdev_numbers;
+
+	ret = cdev_add(&ctx->cdev, ctx->devt, 1);
+	if (ret < 0) {
+		printk(KERN_ERR "%s: cdev_add failed\n", DRIVER_NAME);
+		return -EINVAL;
+	}
+
+	// TODO(sodar): Check parameters?
+	device = device_create(monter_dev_class, NULL, cdev_numbers, NULL,
+				    "monter%u", MINOR(ctx->devt));
+	BUG_ON(device == NULL);
+
+	pci_set_drvdata(dev, ctx);
 
 	return 0;
 }
@@ -42,32 +147,30 @@ monter_pci_probe(struct pci_dev *dev, const struct pci_device_id *id)
 static void
 monter_pci_remove(struct pci_dev *dev)
 {
-	/*
-	 * TODO(sodar):
-	 * - remove assigned character device and free up number slot
-	 */
+	struct monter_device_context *ctx;
 
-	unsigned int *chr_dev_num_ptr;
-	unsigned int chr_dev_num;
+	ctx = pci_get_drvdata(dev);
+	BUG_ON(ctx == NULL);
+	pci_set_drvdata(dev, NULL);
 
-	chr_dev_num_ptr = (unsigned int *)pci_get_drvdata(dev);
-	BUG_ON(chr_dev_num_ptr == NULL);
-
-	chr_dev_num = *chr_dev_num_ptr;
-	kfree(chr_dev_num_ptr);
-
-	printk(KERN_INFO "monter: PCI device remove; chr_dev_num = %u\n", chr_dev_num);
+	device_destroy(monter_dev_class, ctx->devt);
+	cdev_del(&ctx->cdev);
+	kfree(ctx);
 }
 
-static struct pci_device_id monter_pci_device_id = {
+static const struct pci_device_id pci_id_list[] = {
+	{
 	PCI_DEVICE(MONTER_VENDOR_ID, MONTER_DEVICE_ID),
 	.class = 0,
 	.class_mask = 0,
+	},
+	{0, 0, 0},
 };
+MODULE_DEVICE_TABLE(pci, pci_id_list);
 
 static struct pci_driver monter_pci_driver = {
-	.name = "monter",
-	.id_table = &monter_pci_device_id,
+	.name = DRIVER_NAME,
+	.id_table = pci_id_list,
 	.probe = monter_pci_probe,
 	.remove = monter_pci_remove,
 };
@@ -76,13 +179,28 @@ static int __init monter_init_module(void)
 {
 	int ret;
 
-	ret = pci_register_driver(&monter_pci_driver);
+	ret = alloc_chrdev_region(&monter_dev_numbers, 0, MAX_DEVICES, DRIVER_NAME);
 	if (ret < 0) {
-		printk(KERN_INFO "monter: error registering PCI driver\n");
+		printk(KERN_ERR "%s: error on chrdev region allocation\n", DRIVER_NAME);
 		return ret;
 	}
 
-	printk(KERN_INFO "monter: initialised\n");
+	/* NOTE(sodar): Only for debug */
+	printk(KERN_INFO "%s: major=%u minor=%u\n", DRIVER_NAME,
+		MAJOR(monter_dev_numbers), MINOR(monter_dev_numbers));
+
+	monter_dev_class = class_create(THIS_MODULE, DRIVER_NAME);
+	if (IS_ERR(monter_dev_class)) {
+		printk(KERN_ERR "%s: error create device class\n", DRIVER_NAME);
+		return PTR_ERR(monter_dev_class);
+	}
+
+	ret = pci_register_driver(&monter_pci_driver);
+	if (ret < 0) {
+		printk(KERN_ERR "%s: error registering PCI driver\n", DRIVER_NAME);
+		return ret;
+	}
+
 	return 0;
 }
 
@@ -90,7 +208,11 @@ static void __exit monter_exit_module(void)
 {
 	pci_unregister_driver(&monter_pci_driver);
 
-	printk(KERN_INFO "monter: exit\n");
+	if (monter_dev_class)
+		class_destroy(monter_dev_class);
+
+	if (monter_dev_numbers)
+		unregister_chrdev_region(monter_dev_numbers, MAX_DEVICES);
 }
 
 module_init(monter_init_module);
